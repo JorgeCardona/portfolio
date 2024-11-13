b7304d3c454deee080dad09c1ff23305a01c1ce9
Jorge Cardona <jorgecardona@utp.edu.co>
2024-11-13 00:59:49 -0500

    Updated url for backend service

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL    = 'http://127.0.0.1:8000';
-export const API_BASE_URL_CLOUD          = 'https://portfolio-api-server.onrender.com';
+export const API_BASE_URL_LOCAL    = 'http://127.0.0.1:8000';
+export const API_BASE_URL          = 'https://portfolio-api-server.onrender.com';
 export const TABLES_URL            = `${API_BASE_URL}/tables`;
 export const QUERY_URL             = `${API_BASE_URL}/query`;
 export const GENERATE_PASSWORD_URL = `${API_BASE_URL}/generate-password`;
